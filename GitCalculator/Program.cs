8f8559740fa62cdcdfe4be507eeee968268b54e8
AleksiVisuri <aleksi.visuri@edu.bc.fi>
2022-01-27 12:44:27 +0200

    Tehtiin yksinkertainen yhteenlasku laskin

---
+++ GitCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+Console.WriteLine("Tervetuloa calculaattoriin!");
+
+int firstnumber, secondnumber;
+
+Console.WriteLine("Laskemme x + y, anna x:");
+
+firstnumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Anna y:");
+
+secondnumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(firstnumber + " + " + secondnumber + " = " + (firstnumber + secondnumber));
+
+
